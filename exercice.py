fbff615aebedfce96a383106fd89c8b8d126d678
LegrandPamphile <justinldoucet@gmail.com>
2020-09-21 14:28:55 -0400

    Fin

--- exercice.py
+++ exercice.py
@@ -8,20 +8,79 @@ def get_bill(name, data):
 	INDEX_QUANTITY = 1
 	INDEX_PRICE = 2
 
-	return ""
+	TAX_RATE = 0.15
 
+
+	#Calculer le sous-total (somme des items)
+	sum = 0
+	for item in data:
+		sum += item[INDEX_QUANTITY] * item[INDEX_PRICE]
+
+	#Calculer les taxes et total
+	taxes = TAX_RATE * sum
+	total = sum + taxes
+
+	#Retourner la facture formatée (sous-total, taxes, total)
+	result = name
+	result += "\n" + f"SOUS TOTAL {sum : >10.2f} $"
+	result += "\n" + f"TAXES      {taxes : >10.2f} $"
+	result += "\n" + f"TOTAL      {total : >10.2f} $"
+	return result
 
 def format_number(number, num_decimal_digits):
-	return ""
+	#Séparer ls entières et décimales
+	decimal_part = abs(number) % 1.0
+	whole_part = int(abs(number))
+
+	#Formater la partie décimale
+	#C'est magique : decimal_str = f"{decimal_part :.{num_decimal_digits}f}"[1:]
+	decimal_part = str(round(decimal_part * 10**num_decimal_digits))
+	decimal_part = "." + "0" * (num_decimal_digits - len(decimal_part)) + decimal_part
+	#Formater la partie entière
+	whole_part_str = ""
+	while whole_part >= 1000:
+		digits = whole_part % 1000
+		digits_str = f" {digits :0>3}"
+		whole_part_str = digits_str + whole_part_str
+		whole_part //= 1000
+	whole_part_str = str(whole_part) + whole_part_str
+
+	#Concaténer les deux parties
+	if number < 0:
+		whole_part_str = "-" + whole_part_str
+	return whole_part_str + decimal_part
 
 def get_triangle(num_rows):
-	return ""
+	BORDER_CHAR = "+"
+	TRIANGLE_CHAR = "A"
+	#CAlculer la largeur
+	triangle_width = 1 + 2 * (num_rows - 1)
+
+	#Construire la première et dernière ligne
+
+	border_row = BORDER_CHAR * (triangle_width + 2)
+
+	#Afficher le triangle
+	result = border_row
+	#Pour chaque ligne
+	for i in range(num_rows):
+		num_triangle_chars = i * 2 + 1
+		triangle_chars = TRIANGLE_CHAR * num_triangle_chars
+		triangle_line = f"{triangle_chars : ^{triangle_width}}"
+		result += "\n" + BORDER_CHAR + triangle_line + BORDER_CHAR
+	result += "\n" + border_row
+
+	return result
 
 
 if __name__ == "__main__":
 	print(get_bill("Äpik Gämmör", [("chaise", 1, 399.99), ("g-fuel", 69, 35.99)]))
 
+	print(".......................")
+
 	print(format_number(-12345.678, 2))
+
+	print(".......................")
 
 	print(get_triangle(2))
 	print(get_triangle(5))
